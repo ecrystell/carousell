72247fd39e6efa565c716d1eafe706414fdea159
ecrystell <yeosugar6@gmail.com>
2023-11-06 13:54:56 +0800

    working bot

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a)=>{
+    return GatewayIntentBits[a]
+  }),
+});
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+  console.log("I'm in");
+  console.log(client.user.username);
+});
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+client.on("messageCreate", (msg) => {
+  if (msg.author.id != client.user.id) {
+    msg.channel.send(msg.content.split("").reverse().join(""));
   }
-})
+});
 
-client.login("MTE3MDkxNzMzNzQ5NTUxMTEyMA.GKMFxI.YVtFNBEHX0hPeHM7WDe--TU5d2V01lHii-FkbE")
+
+client.login("MTE3MDkxNzMzNzQ5NTUxMTEyMA.GKMFxI.YVtFNBEHX0hPeHM7WDe--TU5d2V01lHii-FkbE");
