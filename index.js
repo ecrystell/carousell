d8e5e1a2069e3bd4682c7c0767b1828356601c26
ecrystell <yeosugar6@gmail.com>
2024-06-08 23:04:44 +0800

    stealth pt2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Client({
   }),
 });
 
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 let prevListings = [];
